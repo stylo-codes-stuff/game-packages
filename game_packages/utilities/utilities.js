39eeb4bb84cf1f9df60b0369eccc16915e3fd076
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2025-05-01 10:23:21 -0500

    Update utilities.js

--- game_packages/utilities/utilities.js
+++ game_packages/utilities/utilities.js
@@ -66,3 +66,9 @@ export function getCirclePoints(array, center, radius) {
     }
     return points
 }
+//generates a number between min and max
+function randomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
